1678368bc0d91c0bdf58f794815881fc25affeef
panquequelol <rene@outlook.my>
2022-10-29 01:55:00 -0300

    fix(header): add routing to nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,8 @@
-import { BookOpenIcon } from "@heroicons/react/24/outline";
+import {
+  BookOpenIcon,
+  MagnifyingGlassCircleIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 function Header() {
   return (
@@ -6,12 +10,18 @@ function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto py-4">
         <div className="flex items-center text-2xl font-black">
           <BookOpenIcon className="h-8 w-8" />
-          <h1>MangaSee</h1>
+          <Link href="/">
+            <h1>MangaSee</h1>
+          </Link>
         </div>
         <nav>
           <ul className="flex gap-4 font-bold">
-            <li>Search</li>
-            <li>Random</li>
+            <li className="bg-white text-black rounded-2xl p-2">
+              <Link href="/search" className="flex gap-2">
+                <span>Search</span>
+                <MagnifyingGlassCircleIcon className="h-6 w-6" />
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
